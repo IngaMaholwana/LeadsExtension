45c5c0340f6e1bb3fa3ffa4a39a8d76f1ce27182
Inga Maholwana <imaholwana023@student.wethinkcode.co.za>
2024-12-18 16:35:01 +0200

    the styling updated to better match application needs

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let myLeads = ["www.dumydata.com","ya ya","www.epiclead.co.za"]
+let myLeads = []
 const inputEl = document.getElementById("input-el")//const is the same as let but it is a constant and it cannot be changed
 const inputBtn = document.getElementById("input-btn") 
 const ulEl = document.getElementById("ul-el")
@@ -6,25 +6,31 @@ const ulEl = document.getElementById("ul-el")
 
 
 
-
 // another way to get clicks from users this one below
 inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value)
-    // myLeads.push(inputEl.value)
-    console.log(myLeads)
+    inputEl.value = "" // thiss so once inputed it be gone
+    renderLeads()
+    
 })
 
-let listItems = "" // list variable list to hold all html
-for (let i =0;i < myLeads.length; i++){
-    listItems += "<li>" + myLeads[i] + "</li>"
-    
+function renderLeads(){
+    let listItems = "" // list variable list to hold all html
+    for (let i =0;i < myLeads.length; i++){
+        // listItems += "<li><a  target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+        //template string can break into multiple lines ` these wei
+        listItems += `
+        <li>
+            <a  target='_blank' href='${myLeads[i] }+ "'> 
+            ${myLeads[i]} 
+            </a>
+        </li>`
 
-    // ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
-    // ulEl.textContent += myLeads[i] + " " this way they will show as a the way written above inner makes em list
-    // anothe alternative to inner
-    // const li = document.createElement("li") //create element
-    // li.textContent = myLeads[i] // set text
-    // ulEl.append(li) //append to ul
-     
+
+        
+    }
+    ulEl.innerHTML = listItems // so it doesnt get manupulated multiple times safer out
+
 }
-ulEl.innerHTML = listItems
+
+
